62ad0aca9988d38aa8ff100e45c58f4f6d9a7081
martin-ratti <martinratti10@gmail.com>
2026-08-26 16:54:36 -0300

    Fix Vite import type error for User

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
-import { User, onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+import type { User } from 'firebase/auth';
 import { auth } from '../firebase';
 
 interface AuthStore {
